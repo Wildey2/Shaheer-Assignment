a877163c1e1478e9ec8362b4d436f7710852e520
Ben Wilde <orangeducktape12@gmail.com>
2017-05-09 14:45:34 -0400

    The start of the game creation

---
+++ Game.py
@@ -0,0 +1,24 @@
+import random
+import time
+import tkinter as tk
+from tkinter import *
+
+
+class App():
+
+    def __init__(self):
+
+
+        root = Tk()
+
+        displayBox = Text(root)
+        displayBox.grid(row=0,column=0)
+
+
+
+
+
+
+        root.mainloop()
+
+app = App()
