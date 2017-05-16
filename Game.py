f696552846b9aa2940c7cde64a0333eacafe84dd
Ben Wilde <orangeducktape12@gmail.com>
2017-05-16 12:40:40 -0400

    Major Changes

--- Game.py
+++ Game.py
@@ -4,22 +4,28 @@ import tkinter as tk
 from tkinter import *
 
 
-class App():
+class App:
 
     def __init__(self):
 
-
         self.root = Tk()
+
+        self.gameC = tk.Canvas(self.root, width=2000, height=600)
+        self.gameC.grid(column=1, row=0)
+        self.gameC.config(bg="black")
+
+        self.world = tk.PhotoImage(file="Map.png")
+        self.world_img = self.gameC.create_image(650,315,image=self.world)
 
         self.displayBox = Text(self.root)
         self.displayBox.grid(row=0,column=0)
+        self.displayBox.insert(END,">>> World Conquest Game")
         self.displayBox.config(state="disable")
 
-        ebox = Entry(self.root)
-        ebox.grid(row=0,column=1)
+        self.ebox = Entry(self.root)
+        self.ebox.grid(row=2,column=0)
 
         def run(*args):
-
 
             self.displayBox.config(state="normal")
 
@@ -27,22 +33,7 @@ class App():
 
             self.displayBox.config(state="disable")
 
-
-
-
         self.root.bind("<k>",run)
-
-
-
-
-
-
-
-
-
-
-
-
 
         self.root.mainloop()
 
